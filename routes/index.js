e4d82212ed1039c2f7ce3c10132b8f3f74452722
nbohannan@gmail.com <nbohannan@gmail.com>
2019-08-02 13:33:53 -0500

    dropdown menu for products

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,20 @@ router.get("/", (req, res) => {
     res.render("index")
 })
 
+router.get("/about", (req, res) => {
+    res.render("about")
+})
+
+router.get("/resources", (req, res) => {
+    res.render("resources")
+})
+
+router.get("/social", (req, res) => {
+    res.render("social")
+})
+
+router.get("/contact", (req, res) => {
+    res.render("contact")
+})
+
 module.exports = router
